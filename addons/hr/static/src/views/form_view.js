bf0b4c65966b8c8789fb16f6ca5b882519ae9ce0
Kevin Baptiste <kba@odoo.com>
2022-10-21 09:41:36 +0000

    [FIX] hr: remove t-on-* from views arch
    Remove the call to `t-on-click` in the arch of form/kanban views as it
    crashes when the view is opened in studio.
    
    Instead use a dedicated `<widget>`.
    
    closes odoo/odoo#103820
    
    X-original-commit: 2a213a150b4421ce4da32db754651cfc41adbef4
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- addons/hr/static/src/views/form_view.js
+++ addons/hr/static/src/views/form_view.js
@@ -4,10 +4,8 @@ import { registry } from '@web/core/registry';
 
 import { formView } from '@web/views/form/form_view';
 import { FormController } from '@web/views/form/form_controller';
-import { FormRenderer } from '@web/views/form/form_renderer';
 
 import { useArchiveEmployee } from '@hr/views/archive_employee_hook';
-import { useOpenChat } from "@mail/views/open_chat_hook";
 
 export class EmployeeFormController extends FormController {
     setup() {
@@ -29,15 +27,7 @@ export class EmployeeFormController extends FormController {
     }
 }
 
-export class EmployeeFormRenderer extends FormRenderer {
-    setup() {
-        super.setup();
-        this.openChat = useOpenChat(this.props.record.resModel);
-    }
-}
-
 registry.category('views').add('hr_employee_form', {
     ...formView,
     Controller: EmployeeFormController,
-    Renderer: EmployeeFormRenderer,
 });
